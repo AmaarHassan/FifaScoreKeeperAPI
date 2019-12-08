54b17cdd0859c65fbff54f3bd704f58c6b9d1e60
AmaarHassan <amaarhassancs@gmail.com>
2019-12-08 20:42:45 +0500

    comment push

--- models/index.js
+++ models/index.js
@@ -1,2 +1,4 @@
 // models are the data access layer to put it straight forward
 // you put all the crud (database related tasks) inside the models alongwith their schemas
+
+// new one line just to push the commit
