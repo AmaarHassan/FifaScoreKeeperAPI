51c96995bad1b0eaddcd77b64aa9cf877155861e
AmaarHassan <amaarhassancs@gmail.com>
2019-12-08 20:01:13 +0500

    setup express with routes and basic schema with validators

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+// the request is directed to some route and the first thing that route does is call a controller
+// function
+
+// controller functions are actually there for the redirection and very small decision making
+// mainly they control the flow 
---
+++ helpers/index.js
@@ -0,0 +1,6 @@
+// helpers are the beautiful functions
+// that is it
+// they are the kind of perfectly reusable functions
+// for example you have figured out a way to convert a json into a mongodb query...that is it
+// it is something to be reckoned with...use that inside helper function
+// help others as well
